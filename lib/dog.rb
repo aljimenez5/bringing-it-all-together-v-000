cbc8771c85bf9eae22547ebda009174219134741
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-09-14 02:21:45 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -49,7 +49,7 @@ class Dog
   def self.find_or_create_by(name:, breed:)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed).flatten
       if !dog.empty?
-        
+        self.new(dog[0])
     
     
   
